f11631b9370265ec42de58f7acee8f92e77a51b5
AhmedTantawy98 <ahmed.tantawy1293@gmail.com>
2024-03-26 07:19:24 +0200

    Edit Images

--- js/master.js
+++ js/master.js
@@ -58,7 +58,7 @@ randomBackEl.forEach((span) => {
     })
 });
 let landingPage= document.querySelector(".landing-page");
-let imgs = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "07.jpg", "08.jpg", "09.jpg", "10.jpg"];
+let imgs = ["02.jpg", "03.jpg", "04.jpg", "05.jpg", "07.jpg", "08.jpg", "09.jpg", "10.jpg"];
 function randomizImgs () {
     if(backgroundOption === true){
         backgroundInterval= setInterval(() => {
